aa803fa8952012bef28d6a0adedf5dc8536bc6cd
Thomas Baron <thomasjbaron@hotmail.com>
2014-06-30 20:23:40 -0600

    Added ability to watch for changes.

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ var path = require('path');
 var fs = require('fs');
 var async = require('async');
 
-exports.bundle = function(config, callback) {
-	fs.readFile(config, function(err, file1) {
-		var cfg = JSON.parse(file1);
-		var allcss = '';
-		var filefuncs = [];
-		for (var i=0; i<cfg.files.length; i++) {
-			(function(f) {
-				filefuncs.push(function(callback2) {
-					fs.readFile(path.dirname(config)+'/'+f, {encoding: 'utf8'}, function(err, file2) {
-						file2 = file2.replace(/\$([a-zA-Z]+)/g, function(a, b, c) {
-							return cfg.values[b];
+exports.bundle = function(config, options, callback) {
+	if (this !== exports.bundle) new exports.bundle(config, options, callback);
+	
+	if (callback === undefined) {
+		callback = options;
+		options = {};
+	}
+
+	function compile() {
+		fs.readFile(config, function(err, file1) {
+			var cfg = JSON.parse(file1);
+			var allcss = '';
+			var filefuncs = [];
+			for (var i=0; i<cfg.files.length; i++) {
+				(function(f) {
+					filefuncs.push(function(callback2) {
+						fs.readFile(path.dirname(config)+'/'+f, {encoding: 'utf8'}, function(err, file2) {
+							file2 = file2.replace(/\$([a-zA-Z]+)/g, function(a, b, c) {
+								return cfg.values[b];
+							});
+							allcss += file2 + '\n';
+							callback2(null, true);
 						});
-						allcss += file2 + '\n';
-						callback2(null, true);
 					});
-				});
-			})(cfg.files[i]);
-		}
-		async.parallel(filefuncs, function(err, res) {
-			callback(null, allcss);
+				})(cfg.files[i]);
+			}
+			async.parallel(filefuncs, function(err, res) {
+				callback(null, allcss);
+			});
 		});
-	});
+	}
+
+	if (options.watch) {
+		fs.watch(config, compile);
+	}
+
+	compile();
 };
